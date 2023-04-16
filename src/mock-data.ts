b69b40d034176ae1b52f29914a7ef9d95c2b3454
a2345sooted <rcschupp@gmail.com>
2023-04-16 01:08:24 -0500

    working

---
+++ src/mock-data.ts
@@ -0,0 +1,46 @@
+export const DIMENSIONS = {
+  tags: [
+    {
+      name: 'cats',
+      source_tags: [
+        'catsofmastodon',
+        'caturday',
+        'cats',
+        'cat',
+        'deepthoughtsofcats'
+      ]
+    },
+    {
+      name: 'weekend',
+      source_tags: [
+        'fensterfreitag',
+        'saturday'
+      ]
+    },
+    {
+      name: 'art',
+      source_tags: [
+        'mastoart'
+      ]
+    },
+    {
+      name: 'introductions',
+      source_tags: [
+        'introduction'
+      ]
+    },
+    {
+      name: 'funny_prompts',
+      source_tags: [
+        'foodasongormovie',
+        'sillycuratorprompts'
+      ]
+    },
+    {
+      name: 'screenshots',
+      source_tags: [
+        'screenshotsaturday'
+      ]
+    }
+  ]
+}
